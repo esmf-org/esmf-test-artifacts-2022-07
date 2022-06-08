14cda09db808a9f8916603728c84eece062f4759
Rocky Dunlap <dunlap@ucar.edu>
2022-06-08 13:55:20 -0700

    update for build of intel_19.1.2_mpi_g_develop with hash v8.4.0b01-2-gf0d4ab2 on cori [ci skip]

---
+++ develop/cori/intel/19.1.2/g/mpi/7.7.19/out/build-intel_19.1.2_mpi_g.bat
@@ -0,0 +1,31 @@
+Wed Jun 8 13:55:20 PDT 2022
+#!/bin/sh -l
+#SBATCH --account=e3sm
+#SBATCH -o build-intel_19.1.2_mpi_g.bat_%j.o
+#SBATCH -e build-intel_19.1.2_mpi_g.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH -C haswell
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module unload darshan
+module load intel/19.1.2.254 cray-mpich/7.7.19 cray-netcdf/4.6.3.2
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBS="-lnetcdf"
+export ESMF_NETCDFF_LIBS="-lnetcdff"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/cray/pe/hdf5/1.10.5.2/INTEL/19.0/lib/pkgconfig
+export ESMF_DIR=/global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.1.2_mpi_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 32 2>&1| tee build_$JOBID.log
+
