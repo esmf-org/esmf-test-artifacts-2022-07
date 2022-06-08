47c0a79ad1b921748ece815ad410830a18aa9828
Rocky Dunlap <dunlap@ucar.edu>
2022-06-08 13:55:29 -0700

    update for build of intel_19.1.2_mpiuni_g_develop with hash v8.4.0b01-2-gf0d4ab2 on cori [ci skip]

---
+++ develop/cori/intel/19.1.2/g/mpiuni/none/out/test-intel_19.1.2_mpiuni_g.bat
@@ -0,0 +1,37 @@
+Wed Jun 8 13:55:28 PDT 2022
+#!/bin/sh -l
+#SBATCH --account=e3sm
+#SBATCH -o test-intel_19.1.2_mpiuni_g.bat_%j.o
+#SBATCH -e test-intel_19.1.2_mpiuni_g.bat_%j.e
+#SBATCH --time=3:00:00
+#SBATCH -C haswell
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module unload darshan
+module load intel/19.1.2.254  cray-netcdf/4.6.3.2
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBS="-lnetcdf"
+export ESMF_NETCDFF_LIBS="-lnetcdff"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/cray/pe/hdf5/1.10.5.2/INTEL/19.0/lib/pkgconfig
+export ESMF_DIR=/global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.1.2_mpiuni_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
