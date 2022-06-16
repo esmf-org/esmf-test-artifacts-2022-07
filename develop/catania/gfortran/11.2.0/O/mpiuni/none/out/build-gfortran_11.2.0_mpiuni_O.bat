f3d08b10782c8fc8b757ba2ca7a8453d6a0a2944
Rocky Dunlap <dunlap@ucar.edu>
2022-06-16 12:41:34 -0600

    update for build of gfortran_11.2.0_mpiuni_O_develop with hash v8.4.0b01-2-gf0d4ab2 on catania [ci skip]

---
+++ develop/catania/gfortran/11.2.0/O/mpiuni/none/out/build-gfortran_11.2.0_mpiuni_O.bat
@@ -0,0 +1,22 @@
+Thu Jun 16 12:41:34 MDT 2022
+#!/bin/bash -l
+export JOBID=12345
+
+module use /project/esmf/stack/modulefiles/compilers
+export ESMF_MPIRUN=/Volumes/esmf/rocky/esmf-testing/gfortran_11.2.0_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
+module load gcc/11.2.0  netcdf/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF=nc-config
+export ESMF_MPILAUNCHOPTIONS=--oversubscribe
+export ESMF_DIR=/Volumes/esmf/rocky/esmf-testing/gfortran_11.2.0_mpiuni_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 4 2>&1| tee build_$JOBID.log
+
---
+++ develop/catania/gfortran/11.2.0/O/mpiuni/none/out/test-gfortran_11.2.0_mpiuni_O.bat
@@ -0,0 +1,27 @@
+Thu Jun 16 12:41:34 MDT 2022
+#!/bin/bash -l
+export JOBID=12346
+
+module use /project/esmf/stack/modulefiles/compilers
+module load gcc/11.2.0  netcdf/4.7.4
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF=nc-config
+export ESMF_MPILAUNCHOPTIONS=--oversubscribe
+export ESMF_DIR=/Volumes/esmf/rocky/esmf-testing/gfortran_11.2.0_mpiuni_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
