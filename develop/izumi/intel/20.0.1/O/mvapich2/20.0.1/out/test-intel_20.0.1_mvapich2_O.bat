29927facb394f9ce71e1bcb186ac0276224dda11
Rocky Dunlap <dunlap@ucar.edu>
2022-06-16 13:05:47 -0600

    update for build of intel_20.0.1_mvapich2_O_develop with hash v8.4.0b01-2-gf0d4ab2 on izumi [ci skip]

---
+++ develop/izumi/intel/20.0.1/O/mvapich2/20.0.1/out/test-intel_20.0.1_mvapich2_O.bat
@@ -0,0 +1,30 @@
+Thu Jun 16 13:05:45 MDT 2022
+#!/bin/sh -l
+#PBS -N test-intel_20.0.1_mvapich2_O.bat
+#PBS -l walltime=2:00:00
+#PBS -q medium
+#PBS -A UNUSED
+#PBS -l nodes=1:ppn=48
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /scratch/cluster/dunlap/esmf-testing/intel_20.0.1_mvapich2_O_develop
+module load compiler/intel/20.0.1 mpi/2.3.3/intel/20.0.1 tool/netcdf/4.7.4/intel/20.0.1
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/cluster/dunlap/esmf-testing/intel_20.0.1_mvapich2_O_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=mvapich2
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
