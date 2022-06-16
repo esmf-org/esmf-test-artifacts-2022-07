a6d8c0ba5f440c69159b4d25bac9f2cff38813d5
Rocky Dunlap <dunlap@ucar.edu>
2022-06-16 13:08:00 -0600

    update for build of gfortran_11.2.0_openmpi_g_develop with hash v8.4.0b01-2-gf0d4ab2 on catania [ci skip]

---
+++ develop/catania/gfortran/11.2.0/g/openmpi/4.1.0/out/build-gfortran_11.2.0_openmpi_g.bat
@@ -0,0 +1,21 @@
+Thu Jun 16 13:08:00 MDT 2022
+#!/bin/bash -l
+export JOBID=12345
+
+module use /project/esmf/stack/modulefiles/compilers
+module load gcc/11.2.0 openmpi/4.1.0 netcdf/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF=nc-config
+export ESMF_MPILAUNCHOPTIONS=--oversubscribe
+export ESMF_DIR=/Volumes/esmf/rocky/esmf-testing/gfortran_11.2.0_openmpi_g_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 4 2>&1| tee build_$JOBID.log
+
