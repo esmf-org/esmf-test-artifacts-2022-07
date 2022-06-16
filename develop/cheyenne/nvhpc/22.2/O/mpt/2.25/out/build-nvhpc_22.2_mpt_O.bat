cd3f3d9da9d3d3c7a9338722444c294b1dd807c1
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-16 02:34:02 -0600

    update for build of nvhpc_22.2_mpt_O_develop with hash v8.4.0b01-2-gf0d4ab2 on cheyenne [ci skip]

---
+++ develop/cheyenne/nvhpc/22.2/O/mpt/2.25/out/build-nvhpc_22.2_mpt_O.bat
@@ -0,0 +1,27 @@
+Thu Jun 16 02:34:00 MDT 2022
+#!/bin/sh -l
+#PBS -N build-nvhpc_22.2_mpt_O.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/dunlap/esmf-testing/nvhpc_22.2_mpt_O_develop
+
+module load python cmake
+module load nvhpc/22.2 mpt/2.25 netcdf/4.8.1
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/nvhpc_22.2_mpt_O_develop
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=mpt
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 36 2>&1| tee build_$JOBID.log
+
