2296462eec024673ed53b804f179721cc10c19bf
Rocky Dunlap <dunlap@ucar.edu>
2022-06-16 16:39:54 -0600

    update for build of nag_6.2_mvapich2_g_develop with hash v8.4.0b01-2-gf0d4ab2 on izumi [ci skip]

---
+++ develop/izumi/nag/6.2/g/mvapich2/6.2/out/build-nag_6.2_mvapich2_g.bat
@@ -0,0 +1,25 @@
+Thu Jun 16 16:39:41 MDT 2022
+#!/bin/sh -l
+#PBS -N build-nag_6.2_mvapich2_g.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=3:00:00
+#PBS -q medium
+#PBS -A UNUSED
+#PBS -l nodes=1:ppn=48
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /scratch/cluster/dunlap/esmf-testing/nag_6.2_mvapich2_g_develop
+module load compiler/nag/6.2-8.1.0 mpi/2.3.3/nag/6.2 tool/netcdf/c4.6.1-f4.4.4/nag-gnu/6.2-8.1.0
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/cluster/dunlap/esmf-testing/nag_6.2_mvapich2_g_develop
+export ESMF_COMPILER=nag
+export ESMF_COMM=mvapich2
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 48 2>&1| tee build_$JOBID.log
+
