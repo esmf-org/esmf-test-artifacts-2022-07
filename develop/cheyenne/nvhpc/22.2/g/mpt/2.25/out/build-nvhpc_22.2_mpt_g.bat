7be328f8005332130f759c643579da9cd8c49acb
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-21 05:03:23 -0600

    update for test of nvhpc_22.2_mpt_g_develop with hash v8.4.0b01-2-gf0d4ab2 on cheyenne [ci skip]

--- develop/cheyenne/nvhpc/22.2/g/mpt/2.25/out/build-nvhpc_22.2_mpt_g.bat
+++ develop/cheyenne/nvhpc/22.2/g/mpt/2.25/out/build-nvhpc_22.2_mpt_g.bat
@@ -1,4 +1,4 @@
-Tue Jun 21 02:48:22 MDT 2022
+Tue Jun 21 05:02:54 MDT 2022
 #!/bin/sh -l
 #PBS -N build-nvhpc_22.2_mpt_g.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/nvhpc/22.2/g/mpt/2.25/out/test-nvhpc_22.2_mpt_g.bat
+++ develop/cheyenne/nvhpc/22.2/g/mpt/2.25/out/test-nvhpc_22.2_mpt_g.bat
@@ -1,4 +1,4 @@
-Tue Jun 21 02:48:22 MDT 2022
+Tue Jun 21 05:02:54 MDT 2022
 #!/bin/sh -l
 #PBS -N test-nvhpc_22.2_mpt_g.bat
 #PBS -l walltime=3:00:00
