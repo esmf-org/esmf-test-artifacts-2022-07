0b57bf8438469d0d277f9a5aa963da2a40641dca
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-22 03:00:34 -0600

    update for test of gfortran_10.1.0_openmpi_O_develop with hash v8.4.0b01-2-gf0d4ab2 on cheyenne [ci skip]

--- develop/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/build-gfortran_10.1.0_openmpi_O.bat
+++ develop/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/build-gfortran_10.1.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Jun 22 02:20:10 MDT 2022
+Wed Jun 22 02:59:57 MDT 2022
 #!/bin/sh -l
 #PBS -N build-gfortran_10.1.0_openmpi_O.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_O.bat
+++ develop/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Jun 22 02:20:09 MDT 2022
+Wed Jun 22 02:59:57 MDT 2022
 #!/bin/sh -l
 #PBS -N test-gfortran_10.1.0_openmpi_O.bat
 #PBS -l walltime=2:00:00
