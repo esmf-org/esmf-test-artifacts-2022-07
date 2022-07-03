60020253e52ac09111175099e3a59ad01cbd033b
Mark.Potts <Mark.Potts@noaa.gov>
2022-07-03 01:58:05 -0400

    update for test of intel_19.1.3_intelmpi_O_develop with hash v8.4.0b02-7-g9328c9b on discover [ci skip]

--- develop/discover/intel/19.1.3/O/intelmpi/19.1.3.304/out/build-intel_19.1.3_intelmpi_O.bat
+++ develop/discover/intel/19.1.3/O/intelmpi/19.1.3.304/out/build-intel_19.1.3_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sun Jul 3 01:18:16 EDT 2022
+Sun Jul 3 01:57:23 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_19.1.3_intelmpi_O.bat_%j.o
--- develop/discover/intel/19.1.3/O/intelmpi/19.1.3.304/out/test-intel_19.1.3_intelmpi_O.bat
+++ develop/discover/intel/19.1.3/O/intelmpi/19.1.3.304/out/test-intel_19.1.3_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sun Jul 3 01:18:16 EDT 2022
+Sun Jul 3 01:57:22 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_19.1.3_intelmpi_O.bat_%j.o
