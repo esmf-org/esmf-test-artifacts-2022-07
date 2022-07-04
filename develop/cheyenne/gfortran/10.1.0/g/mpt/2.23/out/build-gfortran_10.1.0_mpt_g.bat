0d67b67488b01ddd86ffbfb3f2d18f6dba2a8c41
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-07-04 02:30:38 -0600

    update for build of gfortran_10.1.0_mpt_g_develop with hash v8.4.0b02-7-g9328c9b on cheyenne [ci skip]

--- develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/build-gfortran_10.1.0_mpt_g.bat
+++ develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/build-gfortran_10.1.0_mpt_g.bat
@@ -1,4 +1,4 @@
-Sun Jul 3 03:06:12 MDT 2022
+Mon Jul 4 02:30:36 MDT 2022
 #!/bin/sh -l
 #PBS -N build-gfortran_10.1.0_mpt_g.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/test-gfortran_10.1.0_mpt_g.bat
+++ develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/test-gfortran_10.1.0_mpt_g.bat
@@ -1,4 +1,4 @@
-Sun Jul 3 03:06:13 MDT 2022
+Mon Jul 4 02:30:36 MDT 2022
 #!/bin/sh -l
 #PBS -N test-gfortran_10.1.0_mpt_g.bat
 #PBS -l walltime=2:00:00
