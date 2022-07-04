923d312e6de6446c9a61be1b7bb486d4100d750a
Mark A Potts <Mark.Potts@noaa.gov>
2022-07-04 01:24:44 -0400

    update for test of intel_2019.5_mpiuni_O_develop with hash v8.4.0b02-7-g9328c9b on gaea [ci skip]

--- develop/gaea/intel/2019.5/O/mpiuni/none/out/build-intel_2019.5_mpiuni_O.bat
+++ develop/gaea/intel/2019.5/O/mpiuni/none/out/build-intel_2019.5_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Mon Jul 4 00:39:09 EDT 2022
+Mon Jul 4 01:23:57 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o build-intel_2019.5_mpiuni_O.bat_%j.o
--- develop/gaea/intel/2019.5/O/mpiuni/none/out/test-intel_2019.5_mpiuni_O.bat
+++ develop/gaea/intel/2019.5/O/mpiuni/none/out/test-intel_2019.5_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Mon Jul 4 00:39:09 EDT 2022
+Mon Jul 4 01:23:58 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o test-intel_2019.5_mpiuni_O.bat_%j.o
