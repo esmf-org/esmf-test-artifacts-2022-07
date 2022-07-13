51eb212760df9ad5506caf05eda03c07c0a8cb7e
Mark A Potts <Mark.Potts@noaa.gov>
2022-07-13 00:43:26 -0400

    update for build of intel_2019.5_mpi_g_develop with hash v8.4.0b03-3-g14a79d5 on gaea [ci skip]

--- develop/gaea/intel/2019.5/g/mpi/7.7.11/out/build-intel_2019.5_mpi_g.bat
+++ develop/gaea/intel/2019.5/g/mpi/7.7.11/out/build-intel_2019.5_mpi_g.bat
@@ -1,4 +1,4 @@
-Tue Jul 12 01:43:20 EDT 2022
+Wed Jul 13 00:43:26 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o build-intel_2019.5_mpi_g.bat_%j.o
--- develop/gaea/intel/2019.5/g/mpi/7.7.11/out/test-intel_2019.5_mpi_g.bat
+++ develop/gaea/intel/2019.5/g/mpi/7.7.11/out/test-intel_2019.5_mpi_g.bat
@@ -1,4 +1,4 @@
-Tue Jul 12 01:43:20 EDT 2022
+Wed Jul 13 00:43:25 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o test-intel_2019.5_mpi_g.bat_%j.o
