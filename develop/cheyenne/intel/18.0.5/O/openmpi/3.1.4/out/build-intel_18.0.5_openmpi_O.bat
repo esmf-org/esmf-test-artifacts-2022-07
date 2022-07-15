2e35d68f9d6475fc191987beadf7511133d0cf12
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-07-15 02:50:35 -0600

    update for build of intel_18.0.5_openmpi_O_develop with hash v8.4.0b04-2-gc831a7c on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
+++ develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
@@ -1,4 +1,4 @@
-Thu Jul 14 04:29:37 MDT 2022
+Fri Jul 15 02:50:34 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_openmpi_O.bat
 #PBS -l walltime=2:00:00
--- develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
+++ develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
@@ -1,4 +1,4 @@
-Thu Jul 14 04:29:37 MDT 2022
+Fri Jul 15 02:50:35 MDT 2022
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_openmpi_O.bat
 #PBS -l walltime=3:00:00
