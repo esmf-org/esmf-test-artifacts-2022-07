67eda239f751404cf47ac686d89ae6725e587f2d
Mark Potts <mpotts@redlineperf.com>
2022-07-16 02:38:54 -0500

    update for build of intel_2020.2_intelmpi_O_feature_named-alias with hash v8.4.0b04-8-g9865973 on orion [ci skip]

---
+++ feature_named-alias/orion/intel/2020.2/O/intelmpi/2020.2/out/build-intel_2020.2_intelmpi_O.bat
@@ -0,0 +1,31 @@
+Sat Jul 16 02:38:53 CDT 2022
+#!/bin/sh -l
+#SBATCH --account=da-cpu
+#SBATCH -o build-intel_2020.2_intelmpi_O.bat_%j.o
+#SBATCH -e build-intel_2020.2_intelmpi_O.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load intelpython3 cmake
+export ESMF_MPIRUN=mpirun.srun
+export ESMPY_MPIRUN=srun
+module load intel/2020.2 impi/2020.2 netcdf/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_O_feature_named-alias
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
