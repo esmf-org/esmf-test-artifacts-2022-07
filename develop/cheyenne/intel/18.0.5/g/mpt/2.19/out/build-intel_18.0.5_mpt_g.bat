e137c973a1a499aaa8c8cd9897c2f4043daa09ad
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-07-16 04:37:02 -0600

    update for test of intel_18.0.5_mpt_g_develop with hash v8.4.0b04-2-gc831a7c on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/build-intel_18.0.5_mpt_g.bat
+++ develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/build-intel_18.0.5_mpt_g.bat
@@ -1,4 +1,4 @@
-Sat Jul 16 03:08:03 MDT 2022
+Sat Jul 16 04:36:34 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_mpt_g.bat
 #PBS -l walltime=2:00:00
--- develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/test-intel_18.0.5_mpt_g.bat
+++ develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/test-intel_18.0.5_mpt_g.bat
@@ -1,4 +1,4 @@
-Sat Jul 16 03:08:03 MDT 2022
+Sat Jul 16 04:36:34 MDT 2022
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_mpt_g.bat
 #PBS -l walltime=3:00:00
