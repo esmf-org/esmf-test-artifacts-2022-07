a1d15ac75b80f101ea070861411c000d3edd5836
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-07-16 02:34:02 -0600

    update for build of gfortran_9.1.0_mpt_g_feature_named-alias with hash v8.4.0b04-8-g9865973 on cheyenne [ci skip]

---
+++ feature_named-alias/cheyenne/gfortran/9.1.0/g/mpt/2.22/out/build-gfortran_9.1.0_mpt_g.bat
@@ -0,0 +1,28 @@
+Sat Jul 16 02:34:01 MDT 2022
+#!/bin/sh -l
+#PBS -N build-gfortran_9.1.0_mpt_g.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=2:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/dunlap/esmf-testing/gfortran_9.1.0_mpt_g_feature_named-alias
+
+module load cmake
+module load gnu/9.1.0 mpt/2.22 netcdf/4.7.3
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_F90COMPILER=mpif90
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/gfortran_9.1.0_mpt_g_feature_named-alias
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 36 2>&1| tee build_$JOBID.log
+
