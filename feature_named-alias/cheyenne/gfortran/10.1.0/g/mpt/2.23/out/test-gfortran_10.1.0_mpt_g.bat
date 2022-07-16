ae7849b666c18bc062dadeda3c88c448a0ba0b5f
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-07-16 02:36:10 -0600

    update for build of gfortran_10.1.0_mpt_g_feature_named-alias with hash v8.4.0b04-8-g9865973 on cheyenne [ci skip]

---
+++ feature_named-alias/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/test-gfortran_10.1.0_mpt_g.bat
@@ -0,0 +1,33 @@
+Sat Jul 16 02:36:08 MDT 2022
+#!/bin/sh -l
+#PBS -N test-gfortran_10.1.0_mpt_g.bat
+#PBS -l walltime=2:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/dunlap/esmf-testing/gfortran_10.1.0_mpt_g_feature_named-alias
+
+module load cmake
+module load gnu/10.1.0 mpt/2.23 netcdf/4.7.4
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_F90COMPILER=mpif90
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/gfortran_10.1.0_mpt_g_feature_named-alias
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
