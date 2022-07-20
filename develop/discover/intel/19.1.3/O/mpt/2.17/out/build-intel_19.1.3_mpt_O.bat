5e5a3aa20e77af25fa4cdf61f88788daaf147b21
Mark.Potts <Mark.Potts@noaa.gov>
2022-07-20 01:20:36 -0400

    update for build of intel_19.1.3_mpt_O_develop with hash v8.4.0b06-4-gd14c497 on discover [ci skip]

--- develop/discover/intel/19.1.3/O/mpt/2.17/out/build-intel_19.1.3_mpt_O.bat
+++ develop/discover/intel/19.1.3/O/mpt/2.17/out/build-intel_19.1.3_mpt_O.bat
@@ -1,4 +1,4 @@
-Tue Jul 19 01:54:47 EDT 2022
+Wed Jul 20 01:20:35 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_19.1.3_mpt_O.bat_%j.o
--- develop/discover/intel/19.1.3/O/mpt/2.17/out/test-intel_19.1.3_mpt_O.bat
+++ develop/discover/intel/19.1.3/O/mpt/2.17/out/test-intel_19.1.3_mpt_O.bat
@@ -1,4 +1,4 @@
-Tue Jul 19 01:54:47 EDT 2022
+Wed Jul 20 01:20:36 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_19.1.3_mpt_O.bat_%j.o
