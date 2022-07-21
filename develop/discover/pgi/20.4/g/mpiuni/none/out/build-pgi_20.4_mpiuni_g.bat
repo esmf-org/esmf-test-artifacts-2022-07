55abf1d23f34061ee62b0d8b63e6101950eeaeae
Mark.Potts <Mark.Potts@noaa.gov>
2022-07-21 03:20:46 -0400

    update for test of pgi_20.4_mpiuni_g_develop with hash v8.4.0b06-6-g1266599 on discover [ci skip]

--- develop/discover/pgi/20.4/g/mpiuni/none/out/build-pgi_20.4_mpiuni_g.bat
+++ develop/discover/pgi/20.4/g/mpiuni/none/out/build-pgi_20.4_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Thu Jul 21 01:55:41 EDT 2022
+Thu Jul 21 03:20:32 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-pgi_20.4_mpiuni_g.bat_%j.o
--- develop/discover/pgi/20.4/g/mpiuni/none/out/test-pgi_20.4_mpiuni_g.bat
+++ develop/discover/pgi/20.4/g/mpiuni/none/out/test-pgi_20.4_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Thu Jul 21 01:55:41 EDT 2022
+Thu Jul 21 03:20:31 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-pgi_20.4_mpiuni_g.bat_%j.o
