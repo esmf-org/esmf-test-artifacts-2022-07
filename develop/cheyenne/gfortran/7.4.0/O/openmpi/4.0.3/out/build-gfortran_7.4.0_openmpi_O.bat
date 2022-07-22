45bf922d636d33dc04f053b93ab9e3e123203804
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-07-22 03:21:44 -0600

    update for build of intel_18.0.5_mpt_O_develop with hash v8.4.0b07 on cheyenne [ci skip]

--- develop/cheyenne/gfortran/7.4.0/O/openmpi/4.0.3/out/build-gfortran_7.4.0_openmpi_O.bat
+++ develop/cheyenne/gfortran/7.4.0/O/openmpi/4.0.3/out/build-gfortran_7.4.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Fri Jul 22 02:25:18 MDT 2022
+Fri Jul 22 03:20:34 MDT 2022
 #!/bin/sh -l
 #PBS -N build-gfortran_7.4.0_openmpi_O.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/intel/18.0.5/O/mpt/2.19/out/build-intel_18.0.5_mpt_O.bat
+++ develop/cheyenne/intel/18.0.5/O/mpt/2.19/out/build-intel_18.0.5_mpt_O.bat
@@ -1,4 +1,4 @@
-Thu Jul 21 04:13:59 MDT 2022
+Fri Jul 22 03:21:35 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_mpt_O.bat
 #PBS -l walltime=2:00:00
